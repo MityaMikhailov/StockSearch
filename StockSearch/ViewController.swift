328735d373d10ac3991859515c79486e44db1dcb
Mitya <158564922+MityaMikhailov@users.noreply.github.com>
2024-07-09 18:01:12 +0500

    Initial Commit

---
+++ StockSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockSearch
+//
+//  Created by Dmitriy Mikhailov on 09.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
